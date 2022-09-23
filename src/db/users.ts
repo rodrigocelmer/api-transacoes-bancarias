63f34744ed520cd4adf3417fc4ef5e5af7b1f4e1
FabioLuz87 <fabioddh@hotmail.com>
2022-09-22 21:57:45 -0300

    Fixed user bug

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,11 +1,3 @@
 import { User } from "../model/User";
 
-export const usersDB: User[] = [
-    {
-        "_id": "0",
-        "_name": "Rodrigo",
-        "_cpf": "11122233345",
-        "_email": "rodrigo@gmail.com",
-        "_age": 27
-    }
-];
+export const usersDB: User[] = [];
