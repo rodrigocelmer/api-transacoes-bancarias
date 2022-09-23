d2baf79271b3f41c9be224bb70f6534235144ddc
rodrigocelmer <rodrigocelmer@hotmail.com>
2022-09-22 21:51:03 -0300

    add dummy user - not working

--- src/db/users.ts
+++ src/db/users.ts
@@ -1,3 +1,11 @@
 import { User } from "../model/User";
 
-export const usersDB: User[] = [];
+export const usersDB: User[] = [
+    {
+        "_id": "0",
+        "_name": "Rodrigo",
+        "_cpf": "11122233345",
+        "_email": "rodrigo@gmail.com",
+        "_age": 27
+    }
+];
